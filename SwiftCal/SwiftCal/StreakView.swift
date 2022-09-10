f23a74e30223990bcdf197ed2e841d1113f029b9
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-09-10 19:32:00 +0200

    feat(StreakView) Add simple logic to calculate the streak for the current month.

--- SwiftCal/SwiftCal/StreakView.swift
+++ SwiftCal/SwiftCal/StreakView.swift
@@ -6,8 +6,18 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct StreakView: View {
+	
+	@FetchRequest(
+		sortDescriptors: [NSSortDescriptor(keyPath: \Day.date, ascending: true)],
+		predicate: NSPredicate(format: "(date >= %@) AND (date <= %@)",
+							   Date().startOfMonth as CVarArg,
+							   Date().endOfMonth as CVarArg),
+		animation: .default)
+	private var days: FetchedResults<Day>
+	
 	
 	@State private var streakValue = 0
 	
@@ -22,7 +32,30 @@ struct StreakView: View {
 				.fontWeight(.heavy)
 				.foregroundColor(streakValue > 0 ? .teal : .pink)
 		}
+		.onAppear {
+			streakValue = calculateStreakValue()
+		}
     }
+	
+	func calculateStreakValue() -> Int {
+		
+		guard !days.isEmpty else { return 0 }
+		
+		let nonFutureDays = days.filter { $0.date!.dayInt <= Date().dayInt }
+		
+		var streakCount = 0
+		for day in nonFutureDays.reversed() {
+			if day.didStudy {
+				streakCount += 1
+			} else {
+				if day.date!.dayInt != Date().dayInt {
+					break
+				}
+			}
+		}
+		
+		return streakCount
+	}
 }
 
 struct StreakView_Previews: PreviewProvider {
