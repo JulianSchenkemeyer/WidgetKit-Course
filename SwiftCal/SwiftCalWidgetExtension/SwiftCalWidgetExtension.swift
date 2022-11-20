d785ffe3bad68ad2d7c216ef1c2dcc3827fa8ae5
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-11-20 15:50:50 +0100

    Add lockscreen widgets for SwiftCal

--- SwiftCal/SwiftCalWidgetExtension/SwiftCalWidgetExtension.swift
+++ SwiftCal/SwiftCalWidgetExtension/SwiftCalWidgetExtension.swift
@@ -57,49 +57,24 @@ struct CalendarEntry: TimelineEntry {
 }
 
 struct SwiftCalWidgetExtensionEntryView : View {
+	@Environment(\.widgetFamily) var family
+
 	var entry: Provider.Entry
-	let columns = Array(repeating: GridItem(.flexible()), count: 7)
 	
 	var body: some View {
-		HStack {
-			Link(destination: URL(string: "streaks")!) {
-				VStack {
-					Text("\(calculateStreakValue())")
-						.font(.system(size: 70, weight: .bold, design: .rounded))
-						.foregroundColor(.teal)
-					Text("Day Streak")
-						.font(.caption)
-						.foregroundColor(.secondary)
-				}
-			}
-			
-			Link(destination: URL(string: "calendar")!) {
-				VStack {
-					CalendarHeaderView(font: .caption)
-					
-					LazyVGrid(columns: columns, spacing: 7) {
-						ForEach(entry.days) { day in
-							if day.date!.monthInt != Date().monthInt {
-								Text("")
-							} else {
-								Text(day.date!.formatted(.dateTime.day()))
-									.font(.caption2)
-									.bold()
-									.frame(maxWidth: .infinity)
-									.foregroundColor(day.didStudy ? .teal : .secondary)
-									.background(
-										Circle()
-											.foregroundColor(.teal.opacity(day.didStudy ? 0.3 : 0.0))
-											.scaleEffect(1.4)
-									)
-							}
-						}
-					}
-				}
-				.padding(.leading, 6)
-			}
+
+		switch family {
+		case .systemMedium:
+			MediumCalendarView(entry: entry, streakValue: calculateStreakValue())
+		case .accessoryCircular:
+			LockScreenCircularView(entry: entry, currentStreak: calculateStreakValue())
+		case .accessoryInline:
+			Label("Streak - \(calculateStreakValue()) days", systemImage: "swift")
+		case .systemSmall, .systemLarge, .systemExtraLarge, .accessoryRectangular:
+			EmptyView()
+		@unknown default:
+			EmptyView()
 		}
-		.padding()
 	}
 	
 	func calculateStreakValue() -> Int {
@@ -133,13 +108,85 @@ struct SwiftCalWidgetExtension: Widget {
 		}
 		.configurationDisplayName("Swift Study Calendar")
 		.description("Track days you study Swift")
-		.supportedFamilies([.systemMedium])
+		.supportedFamilies([.systemMedium,
+							.accessoryRectangular,
+							.accessoryCircular,
+							.accessoryInline])
 	}
 }
 
 struct SwiftCalWidgetExtension_Previews: PreviewProvider {
 	static var previews: some View {
 		SwiftCalWidgetExtensionEntryView(entry: CalendarEntry(date: Date(), days: []))
-			.previewContext(WidgetPreviewContext(family: .systemMedium))
+			.previewContext(WidgetPreviewContext(family: .accessoryCircular))
+	}
+}
+
+//MARK: UI Code for the different Widgets
+
+private struct MediumCalendarView: View {
+	let columns = Array(repeating: GridItem(.flexible()), count: 7)
+
+	var entry: CalendarEntry
+	var streakValue: Int
+
+	var body: some View {
+		HStack {
+			Link(destination: URL(string: "streaks")!) {
+				VStack {
+					Text("\(streakValue)")
+						.font(.system(size: 70, weight: .bold, design: .rounded))
+						.foregroundColor(.teal)
+					Text("Day Streak")
+						.font(.caption)
+						.foregroundColor(.secondary)
+				}
+			}
+
+			Link(destination: URL(string: "calendar")!) {
+				VStack {
+					CalendarHeaderView(font: .caption)
+
+					LazyVGrid(columns: columns, spacing: 7) {
+						ForEach(entry.days) { day in
+							if day.date!.monthInt != Date().monthInt {
+								Text("")
+							} else {
+								Text(day.date!.formatted(.dateTime.day()))
+									.font(.caption2)
+									.bold()
+									.frame(maxWidth: .infinity)
+									.foregroundColor(day.didStudy ? .teal : .secondary)
+									.background(
+										Circle()
+											.foregroundColor(.teal.opacity(day.didStudy ? 0.3 : 0.0))
+											.scaleEffect(1.4)
+									)
+							}
+						}
+					}
+				}
+				.padding(.leading, 6)
+			}
+		}
+		.padding()
+	}
+}
+
+private struct LockScreenCircularView: View {
+	var entry: CalendarEntry
+	var currentStreak: Int
+
+	var daysInCurrentMonth: Int {
+		entry.days.filter { $0.date?.monthInt == Date().monthInt }.count
+	}
+
+	var body: some View {
+		Gauge(value: Double(currentStreak), in: 1...Double(entry.days.count)) {
+			Image(systemName: "swift")
+		} currentValueLabel: {
+			Text("\(currentStreak)")
+		}
+		.gaugeStyle(.accessoryCircular)
 	}
 }
